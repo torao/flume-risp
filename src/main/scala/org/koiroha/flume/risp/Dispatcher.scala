dbc926fc5abc07b1fc9f160ed9828189da5a374b
Takami Torao <t_takami@okwave.co.jp>
2015-08-27 20:11:43 +0900

    test complete

--- src/main/scala/org/koiroha/flume/risp/Dispatcher.scala
+++ src/main/scala/org/koiroha/flume/risp/Dispatcher.scala
@@ -6,18 +6,17 @@
 package org.koiroha.flume.risp
 
 import java.net.SocketAddress
-import java.util.concurrent.atomic.AtomicReference
 
 import com.google.protobuf.ByteString
+import org.apache.flume.Event
 import org.glassfish.grizzly.CompletionHandler
 import org.glassfish.grizzly.http.server.HttpServer
 import org.glassfish.grizzly.websockets._
-import org.apache.flume.Event
+import org.koiroha.flume.risp.Dispatcher.FlumeApp
 import org.slf4j.LoggerFactory
 
-import scala.annotation.tailrec
 import scala.collection.JavaConversions._
-import scala.concurrent.{Promise, Future}
+import scala.concurrent.{Future, Promise}
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Dispatcher
@@ -29,55 +28,21 @@ private class Dispatcher(val address:SocketAddress, val path:String) {
 
   import Dispatcher.logger
 
-  private[this] val websockets = new AtomicReference(Seq[WebSocket]())
-
-  private[this] val app = new WebSocketApplication {
-    @tailrec
-    override def onConnect(ws: WebSocket): Unit = {
-      val current = websockets.get()
-      if (websockets.compareAndSet(current, current :+ ws)) {
-        logger.debug(s"new connection: $ws")
-        super.onConnect(ws)
-      } else {
-        onConnect(ws)
-      }
-    }
-
-    @tailrec
-    override def onClose(ws: WebSocket, frame: DataFrame): Unit = {
-      val current = websockets.get()
-      if (websockets.compareAndSet(current, current.filter {
-        _ != ws
-      })) {
-        logger.debug(s"connection closed: $ws")
-        super.onClose(ws, frame)
-      } else {
-        onClose(ws, frame)
-      }
-    }
-
-    protected override def onError(webSocket: WebSocket, t: Throwable): Boolean = {
-      logger.debug(s"onError($webSocket)", t)
-      super.onError(webSocket, t)
-    }
-  }
+  private[this] val app = new FlumeApp()
 
   private[this] val server = {
     val s = HttpServer.createSimpleServer("", address)
     val addon = new WebSocketAddOn()
     s.getListeners.foreach{ _.registerAddOn(addon) }
     WebSocketEngine.getEngine.register("", path, app)
-    logger.debug(s"server http://$address$path")
     s
   }
 
   def start():Unit = {
     server.start()
-    logger.debug(s"start")
   }
 
   def shutdown():Future[Unit] = {
-    logger.debug(s"shutdown")
     val promise = Promise[Unit]()
     val f = server.shutdown()
     f.addCompletionHandler(new CompletionHandler[HttpServer] {
@@ -94,10 +59,28 @@ private class Dispatcher(val address:SocketAddress, val path:String) {
       builder.addHeaders(RispEvent.FlumeEvent.HeaderField.newBuilder().setName(key).setValue(value).build())
       builder
     }.setBody(ByteString.copyFrom(event.getBody)).build().toByteArray
-    websockets.get().foreach{  _.send(dataframe) }
+    app.send(dataframe)
   }
 }
 
 object Dispatcher {
   private[Dispatcher] val logger = LoggerFactory.getLogger(classOf[Dispatcher])
+
+  private[Dispatcher] class FlumeApp extends WebSocketApplication {
+    def send(msg:Array[Byte]):Unit = {
+      getWebSockets.foreach{ _.send(msg) }
+    }
+    override def onMessage(ws:WebSocket, text:String) = {
+      logger.info(s"onMessage($ws, $text)")
+      super.onMessage(ws, text)
+    }
+    override def onMessage(ws:WebSocket, bytes:Array[Byte]) = {
+      logger.info(s"onMessage($ws, ${bytes.length} bytes)")
+      super.onMessage(ws, bytes)
+    }
+    protected override def onError(webSocket: WebSocket, t: Throwable): Boolean = {
+      logger.error(s"onError($webSocket)", t)
+      super.onError(webSocket, t)
+    }
+  }
 }
--- src/test/scala/org/koiroha/flume/risp/DispatcherSpec.scala
+++ src/test/scala/org/koiroha/flume/risp/DispatcherSpec.scala
@@ -11,6 +11,7 @@ import java.util.function.Consumer
 
 import org.apache.flume.Event
 import org.apache.flume.event.SimpleEvent
+import org.slf4j.LoggerFactory
 import org.specs2.Specification
 
 import scala.concurrent.{Promise, Await}
@@ -24,11 +25,15 @@ import scala.collection.JavaConversions._
  * @author Takami Torao
  */
 class DispatcherSpec extends Specification { def is = s2"""
-Dispatcher should:
-not send event without any connections. $st
+can send/receive signle event. $singleEventDispatch
 """
 
-  def st = {
+  val logger = LoggerFactory.getLogger(classOf[DispatcherSpec])
+
+  def singleEventDispatch = {
+    logger.info("*** standard single even deliver")
+
+    // start dispatcher
 		val host = "localhost"
 		val port = 8011
 		val path = "/api/1.0/logs"
@@ -36,25 +41,37 @@ not send event without any connections. $st
 	  val dispatcher = new Dispatcher(addr, path)
 	  dispatcher.start()
 
+    // create flume event
 		val headers = Map("a" -> "b", "x" -> "y")
 		val body = "hello, world".getBytes(StandardCharsets.UTF_8)
 		val expected = new SimpleEvent()
 		expected.setHeaders(headers)
 		expected.setBody(body)
 
+    // start receiver
 		val promise = Promise[Event]()
-		val receiver = new Receiver(URI.create(s"http://$host:$port$path"), new Consumer[Event]{
+		val receiver = new Receiver(URI.create(s"ws://$host:$port$path"), new Consumer[Event]{
 			override def accept(t:Event): Unit = promise.success(t)
 		}, 30 * 1000)
 
-	  dispatcher.deliver(new SimpleEvent())
+    // send flume event
+	  dispatcher.deliver(expected)
 
+    // wait to receive flume event and shutdown client and server
 		val actual = Await.result(promise.future, Duration.Inf)
 	  Await.result(dispatcher.shutdown(), Duration.Inf)
 		receiver.close()
 
+    logger.info("expected:\n" + expected.toDebug)
+    logger.info("actual:\n" + actual.toDebug)
 		(expected.getHeaders.size() === actual.getHeaders.size()) and
 			actual.getHeaders.foldLeft("" === ""){ case (a, b) => a and (b._2 === expected.getHeaders.apply(b._1))} and
 		  actual.getBody === expected.getBody
+  }
+
+  implicit class _Event(e:Event) {
+    def toDebug:String = {
+      e.getHeaders.map{ case (key, value) => s"$key:$value" }.mkString(",") + s";${new String(e.getBody, StandardCharsets.UTF_8)}"
+    }
   }
 }
